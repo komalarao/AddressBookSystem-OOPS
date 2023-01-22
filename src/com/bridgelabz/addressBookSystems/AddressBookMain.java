20f1cee374b85d9366733dda96339e05e8b0d305
komalarao <manoharvakamullu1999@gmail.com>
2023-01-22 10:43:46 -0800

    refactor: addressBook program to edit existing contact

--- src/com/bridgelabz/addressBookSystems/AddressBookMain.java
+++ src/com/bridgelabz/addressBookSystems/AddressBookMain.java
@@ -2,35 +2,38 @@ package com.bridgelabz.addressBookSystems;
 import java.util.Scanner;
 public class AddressBookMain {
 		public static void main(String[] args) {
-			System.out.println("Welcome To Address Book"); 
+			System.out.println("Welcome To Address Book"); // print welcome massage
 			AddressBook addressBook = new AddressBook();
-			Scanner scanner = new Scanner(System.in); 
+			Scanner scanner = new Scanner(System.in); // Create object
+			// Initialize
 			int Number;
 
 			/**
-			 * Enter number 1 to add this contacts details 
-			 * Enter number 2 to displaying
-			 * contact details
+			 * Enter number 1 to add this contacts details Enter number 2 to displaying
+			 * contact details Enter number 3 to Editing contacts details
 			 */
 			do {
-				System.out.println(" 1. Add Contact \n 2. Display contact \n");
-				System.out.println("Enter the  Number: ");
-
-				Number = scanner.nextInt(); 
+				System.out.println("1. Add Contact \n2. Display contact \n3.EDIT CONTACT");
+				System.out.println("Enter the Number: ");
+				Number = scanner.nextInt();
 
 				switch (Number) {
+
 				case 1:
 					addressBook.addContact();
 					break;
 				case 2:
 					addressBook.showContacts();
 					break;
+				case 3:
+					addressBook.editContact();
+					break;
 				default:
 					System.out.println(" !!!Invalid Input!!! ");
 					break;
 
 				}
-			} while (Number != 3); 
-
+			} while (Number < 4); // calling
 		}
+
 }
