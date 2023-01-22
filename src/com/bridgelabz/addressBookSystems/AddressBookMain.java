f42484f7df8e5f06890a290468961aa8bfc72a37
komalarao <manoharvakamullu1999@gmail.com>
2023-01-22 10:49:10 -0800

    refactor: addressBook program to delete existing contact

--- src/com/bridgelabz/addressBookSystems/AddressBookMain.java
+++ src/com/bridgelabz/addressBookSystems/AddressBookMain.java
@@ -1,39 +1,42 @@
 package com.bridgelabz.addressBookSystems;
 import java.util.Scanner;
 public class AddressBookMain {
-		public static void main(String[] args) {
-			System.out.println("Welcome To Address Book"); // print welcome massage
-			AddressBook addressBook = new AddressBook();
-			Scanner scanner = new Scanner(System.in); // Create object
-			// Initialize
-			int Number;
+	public static void main(String[] args) {
+		System.out.println("Welcome To Address Book"); // print welcome massage
+		AddressBook addressBook = new AddressBook();
+		Scanner scanner = new Scanner(System.in); // Create object
+		// initalize
+		int Number;
 
-			/**
-			 * Enter number 1 to add this contacts details Enter number 2 to displaying
-			 * contact details Enter number 3 to Editing contacts details
-			 */
-			do {
-				System.out.println("1. Add Contact \n2. Display contact \n3.EDIT CONTACT");
-				System.out.println("Enter the Number: ");
-				Number = scanner.nextInt();
+		/**
+		 * Enter number 1 to add this contacts details Enter number 2 to displaying
+		 * contact details Enter number 3 to Editing contacts details Enter number 4 to
+		 * Delete contact
+		 */
+		do {
+			System.out.println(" 1. Add Contact \n2. Display contact \n3.EDIT CONTACT \n4.DELETE CONTACT \n");
+			System.out.println("~Enter the Number~: ");
+			Number = scanner.nextInt();
 
-				switch (Number) {
+			switch (Number) {
 
-				case 1:
-					addressBook.addContact();
-					break;
-				case 2:
-					addressBook.showContacts();
-					break;
-				case 3:
-					addressBook.editContact();
-					break;
-				default:
-					System.out.println(" !!!Invalid Input!!! ");
-					break;
+			case 1:
+				addressBook.addContact();
+				break;
+			case 2:
+				addressBook.showContacts();
+				break;
+			case 3:
+				addressBook.editContact();
+				break;
+			case 4:
+				addressBook.deleteContact();
+				break;
+			default:
+				System.out.println(" !!!Invalid Input!!! ");
+				break;
 
-				}
-			} while (Number < 4); // calling
-		}
-
+			}
+		} while (Number < 4); // calling
+	}
 }
